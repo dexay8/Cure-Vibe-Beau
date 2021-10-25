0b7cf9d6a4f4a06b189aee513ca3f1653e474cf7
Aryan basu <aryanue195034ece@gmail.com>
2021-10-25 19:22:45 +0530

    node setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const path = require('path');
+const app=express();
+app.use(express.static('assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/login.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  app.get('/home',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/homepage.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+const port=process.env.PORT||5000
+ 
+app.listen(port,()=>{
+    console.log(`listening to the port number at ${port}`);
+})
